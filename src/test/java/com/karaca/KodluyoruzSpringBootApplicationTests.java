2a5fc6cb89078df0a661f6a688618b74dbf58d08
Yunus Karaca <ynskrc82@gmail.com>
2022-11-16 21:44:39 +0300

    first commit

---
+++ src/test/java/com/karaca/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
