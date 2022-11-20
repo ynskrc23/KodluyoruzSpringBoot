3014735c5a66c3827e85d8762da64d1327e1e7b9
Yunus Karaca <ynskrc82@gmail.com>
2022-11-20 20:24:16 +0300

    model nesnesi ve listelemesi yapıldı

--- src/main/java/com/karaca/controller/ThymeleafController.java
+++ src/main/java/com/karaca/controller/ThymeleafController.java
@@ -1,9 +1,13 @@
 package com.karaca.controller;
 
+import com.karaca.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
@@ -36,5 +40,32 @@ public class ThymeleafController {
     public String getThymeleaf4Model(Model model){
         model.addAttribute("key_model1","ben model 4'den geldim");
         return "thymeleaf4";
+    }
+
+    //http://localhost:8086/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5Model(Model model){
+        model.addAttribute("key_model1","text");
+        ProductDto productDto = ProductDto
+                .builder()
+                    .productId(0L)
+                    .productName("Ürün adı")
+                    .productPrice(2500)
+                .build();
+        model.addAttribute("key_model2", productDto);
+        return "thymeleaf5";
+    }
+
+    //http://localhost:8086/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6ModelObjectList(Model model) {
+        model.addAttribute("key_model1", "text");
+        List<ProductDto> listem = new ArrayList<>();
+        listem.add(ProductDto.builder().productId(1L).productName("Ürün adı 1").productPrice(1500).build());
+        listem.add(ProductDto.builder().productId(2L).productName("Ürün adı 2").productPrice(2500).build());
+        listem.add(ProductDto.builder().productId(3L).productName("Ürün adı 3").productPrice(3500).build());
+        listem.add(ProductDto.builder().productId(4L).productName("Ürün adı 4").productPrice(4500).build());
+        model.addAttribute("product_list", listem);
+        return "thymeleaf6";
     }
 }
---
+++ src/main/java/com/karaca/dto/BaseDto.java
@@ -0,0 +1,9 @@
+package com.karaca.dto;
+
+import java.util.Date;
+
+public class BaseDto {
+
+    private Long id;
+    private Date date;
+}
